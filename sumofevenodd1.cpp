7be21bc6144e7674d88805a4fd895b5f7ed22e11
saiprasad69 <vnsaiprasadpittala@gmail.com>
2024-10-22 14:07:39 +0530

    Add files via upload

---
+++ sumofevenodd1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{
+	int i=51,sum_even=0,sum_odd=0;
+	for(i=51;i<550;i++)
+	{ if(%2==0)
+	{sum_even=sum_even+i;
+}  
+else
+	sum_odd=sum_odd+i;
+	}
+	printf("sum of even numbers is %d",sum_even);
+	printf("sum of odd numbers is %d",sum_odd);
+	return0;
+}
